5189fb821906418a00b2ed23346b9fe7332c9301
TomaszPronobis <tomasz.p.pronobis@gmail.com>
2025-10-03 12:28:15 +0200

    ollama chat with qwen2.5:3b

---
+++ README.md
@@ -0,0 +1,4 @@
+### Setup
+1. install ollama
+2. pull ollama model (currently `ollama pull qwen2.5:3b`)
+3. `uv run main.py`
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+from ollama import chat
+from ollama import ChatResponse
+
 def main():
-    print("Hello from chefbot!")
+    response: ChatResponse = chat(model='qwen2.5:3b', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+    print(response["message"]["content"])
 
 
 if __name__ == "__main__":
