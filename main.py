9b32c1b4ba3530f16637fc4fee85574a9532a041
Wiktor Pronobis <wiktor.pronobis@gmail.com>
2025-10-03 19:36:04 +0200

    add model variable

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ from ollama import chat
 from ollama import ChatResponse
 from pathlib import Path
 
+MODEL = "qwen3:4b"
+
+
 def main():
-    recipe = Path('./recipes_md/coleslaw_chefkoch.md').read_text(encoding='utf-8')
+    recipe = Path("./recipes_md/coleslaw_chefkoch.md").read_text(encoding="utf-8")
     print(recipe)
     print("#" * 50)
-    response: ChatResponse = chat(model='qwen2.5:3b', messages=[
-  {
-    'role': 'system',
-    'content': 'Du bist ein Experte der Fragen zu dem folgenden Rezept {recipe} beantwortet.'.format(recipe=recipe),
-  },
-  {
-    'role': 'user',
-    'content': 'Wie viel Zucker benötige ich?',
-  },
-])
+    response: ChatResponse = chat(
+        model=MODEL,
+        messages=[
+            {
+                "role": "system",
+                "content": "Du bist ein Experte der Fragen zu dem folgenden Rezept {recipe} beantwortet.".format(
+                    recipe=recipe
+                ),
+            },
+            {
+                "role": "user",
+                "content": "Wie viel Zucker benötige ich?",
+            },
+        ],
+    )
     print(response["message"]["content"])
 
 
