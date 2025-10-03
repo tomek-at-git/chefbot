fbb90d454ddea7b614876aef9800014e29afe987
TomaszPronobis <tomasz.p.pronobis@gmail.com>
2025-10-03 12:38:33 +0200

    ask question about recipe

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from ollama import chat
 from ollama import ChatResponse
+from pathlib import Path
 
 def main():
+    recipe = Path('./recipes_md/coleslaw_chefkoch.md').read_text(encoding='utf-8')
+    print(recipe)
+    print("#" * 50)
     response: ChatResponse = chat(model='qwen2.5:3b', messages=[
   {
+    'role': 'system',
+    'content': 'Du bist ein Experte der Fragen zu dem folgenden Rezept {recipe} beantwortet.'.format(recipe=recipe),
+  },
+  {
     'role': 'user',
-    'content': 'Why is the sky blue?',
+    'content': 'Wie viel Zucker benötige ich?',
   },
 ])
     print(response["message"]["content"])
